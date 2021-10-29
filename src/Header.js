35e1b5838a2c04b64b98c57fb64f6f10f27b7c9c
ak1103dev <ak.apichan1103@gmail.com>
2021-10-29 12:56:12 +0700

    :construction: modify download file

--- src/Header.js
+++ src/Header.js
@@ -2,6 +2,27 @@ import React from "react";
 
 import { ZoomInIcon, ZoomOutIcon, DownloadIcon, CloseIcon, RotateIcon } from "./icons";
 
+const download = e => {
+  console.log(e.target.href);
+  fetch(e.target.href, {
+    method: "GET",
+    headers: {}
+  })
+    .then(response => {
+      response.arrayBuffer().then(function(buffer) {
+        const url = window.URL.createObjectURL(new Blob([buffer]));
+        const link = document.createElement("a");
+        link.href = url;
+        link.setAttribute("download", "image.jpg"); //or any other extension
+        document.body.appendChild(link);
+        link.click();
+      });
+    })
+    .catch(err => {
+      console.log(err);
+    });
+};
+
 const Header = ({
   image,
   alt,
@@ -16,7 +37,7 @@ const Header = ({
   <div className="__react_modal_image__header">
     <span className="__react_modal_image__icon_menu">
       {enableDownload && (
-        <a href={image} download>
+        <a href={image} download onClick={download}>
           <DownloadIcon />
         </a>
       )}
