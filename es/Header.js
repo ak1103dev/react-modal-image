de1110583e126785066be506845261e6dc3ce37f
ak1103dev <ak.apichan1103@gmail.com>
2021-10-29 13:17:39 +0700

    :construction: add lib

---
+++ es/Header.js
@@ -0,0 +1,53 @@
+import React from "react";
+import { ZoomInIcon, ZoomOutIcon, DownloadIcon, CloseIcon, RotateIcon } from "./icons";
+
+var download = function download(e) {
+  console.log(e.target.href);
+  fetch(e.target.href, {
+    method: "GET",
+    headers: {}
+  }).then(function (response) {
+    response.arrayBuffer().then(function (buffer) {
+      var url = window.URL.createObjectURL(new Blob([buffer]));
+      var link = document.createElement("a");
+      link.href = url;
+      link.setAttribute("download", "image.jpg"); //or any other extension
+
+      document.body.appendChild(link);
+      link.click();
+    });
+  })["catch"](function (err) {
+    console.log(err);
+  });
+};
+
+var Header = function Header(_ref) {
+  var image = _ref.image,
+      alt = _ref.alt,
+      zoomed = _ref.zoomed,
+      toggleZoom = _ref.toggleZoom,
+      toggleRotate = _ref.toggleRotate,
+      onClose = _ref.onClose,
+      enableDownload = _ref.enableDownload,
+      enableZoom = _ref.enableZoom,
+      enableRotate = _ref.enableRotate;
+  return React.createElement("div", {
+    className: "__react_modal_image__header"
+  }, React.createElement("span", {
+    className: "__react_modal_image__icon_menu"
+  }, enableDownload && React.createElement("a", {
+    href: image,
+    download: true,
+    onClick: download
+  }, React.createElement(DownloadIcon, null)), enableZoom && React.createElement("a", {
+    onClick: toggleZoom
+  }, zoomed ? React.createElement(ZoomOutIcon, null) : React.createElement(ZoomInIcon, null)), enableRotate && React.createElement("a", {
+    onClick: toggleRotate
+  }, React.createElement(RotateIcon, null)), React.createElement("a", {
+    onClick: onClose
+  }, React.createElement(CloseIcon, null))), alt && React.createElement("span", {
+    className: "__react_modal_image__caption"
+  }, alt));
+};
+
+export default Header;
